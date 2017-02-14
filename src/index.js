816d3cf0b417f00e626818780b6653b1a956a722
marleysid <bsiddhartha25@gmail.com>
2017-02-14 10:44:34 +0545

    signup youtube api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = function(){
+
+const API_KEY = 'AIzaSyBwRtMv0WN-3VJyCbzBd2AAtK-t3HBxJ10';
+
+
+const App = () => {
 	return <div>Hello</div>;
 }
 
